8aa04a08ffb5ef3b50a42683b7e1c714f904f6b0
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2026-05-13 22:47:10 +1000

    fetching data from backend

--- screens/RecentExpense.js
+++ screens/RecentExpense.js
@@ -1,10 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ExpenseOutput from '../components/ExpenseOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpense(){
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    // Fetch expenses from the backend and update the context
+    // You can implement this using your fetchExpenses function and then update the context with the fetched data
+    async function getExpenses(){
+      const expenses =await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
--- util/http.js
+++ util/http.js
@@ -1,7 +1,25 @@
 import axios from "axios";
 
+const BACKEND_URL = 'https://react-native-94e8e-default-rtdb.asia-southeast1.firebasedatabase.app/';  
+
 export function storeExpenses(expenseData) {
-  axios.post('https://react-native-94e8e-default-rtdb.asia-southeast1.firebasedatabase.app//expenses.json',
-    expenseData
-  );
+  axios.post(BACKEND_URL + 'expenses.json', expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + 'expenses.json');
+
+  const expenses =[];
+  for (const key in response.data) {
+    const expenseObject = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+
+    expenses.push(expenseObject);
+  }
+  
+  return expenses;
 }
